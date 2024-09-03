893cd6d9e4991b34dece0b8f690736686fd214b4
Yousef Sayed <95933671+yousefsayedd1@users.noreply.github.com>
2024-09-03 20:29:15 +0300

    Add initial project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+WheatFlour obj = new WheatFlour();
+Console.WriteLine(obj.ID);
+Console.WriteLine(obj.Name);
+Console.WriteLine(obj.InstructionOnPreparing);
+CocoaPowder obj2 = new CocoaPowder();
+Console.WriteLine(obj2.ID);
+Console.WriteLine(obj2.Name);
+Console.WriteLine(obj2.InstructionOnPreparing);
+
+
